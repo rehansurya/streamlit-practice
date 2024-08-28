3195eb983708052853d7261f02dbcc8eaaec4f6f
Rehan Surya <rehansurya111@gmail.com>
2024-08-28 20:13:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ for i, question in enumerate(questions):
     answers[i] = st.text_input(question)
 
 # Submit button
-if st.button("Submit"):
+if st.button(":blue[Submit]"):
     # Validate that all questions are answered
     if all(answers[i] for i in range(len(questions))):
         # Prepare email content
