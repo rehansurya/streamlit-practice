3526c5998c0ecf8c677486e0dedb6b609b165875
Rehan Surya <rehansurya111@gmail.com>
2024-08-28 20:06:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Blog Questionnaire for SEO'):color[Blue] 
+st.title('Blog Questionnaire for SEO:blue[blue]') 
 
 st.markdown('#### Understanding the Company’s Vision and Goals')
 
