d2adc1aed1a4bd0d78791e859af6d1fc7c224b32
Rehan Surya <rehansurya111@gmail.com>
2024-09-04 18:38:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,17 @@ import streamlit as st
 from email.mime.text import MIMEText
 import smtplib
 
-# Add a logo image at the top right
-logo_url = "https://cdn.prod.website-files.com/654cf378c048081b445dcc67/6561f93907af8a7a9a45df15_logo-white.svg"  # Replace with the actual URL or file path of your logo
-row1, row2 = st.rows([1, 3])
-
-with row1:
-    st.image(logo_url, use_column_width=True)
+# Create a container for the title and logo
+with st.container():
+    # Create a row with two columns
+    col1, col2 = st.columns([4, 1])
     
-with row2:
-    st.title('Blog Questionnaire for SEO')
+    with col1:
+        logo_url = "https://cdn.prod.website-files.com/654cf378c048081b445dcc67/6561f93907af8a7a9a45df15_logo-white.svg"  # Replace with the actual URL or file path of your logo
+        st.image(logo_url, use_column_width=True)
+        
+    with col2:
+        st.title('Blog Questionnaire for SEO')
 
 st.markdown('#### Understanding the Company’s Vision and Goals')
 
